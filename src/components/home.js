407eef1560fe0fe87d7002274f0eb731a9682016
Rocky Chima <jatinder.chima@gmail.com>
2019-06-01 15:12:42 -0600

    add plain buttons on home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Home extends Component {
   render() {
@@ -6,6 +7,12 @@ class Home extends Component {
       <div>
         <h1>Success Partners</h1>
         {/* <img alt="logo" src="" /> */}
+        <NavLink to={'/dashboard'}>
+          <button className="login-btn">SIGN UP</button>
+        </NavLink>
+        <NavLink to={'/dashboard'}>
+          <button className="login-btn">LOGIN</button>
+        </NavLink>
       </div>
     );
   }
