a2763b9e8aee5b2e7c3a42fcf620b6f6d9daa57a
Rocky Chima <jatinder.chima@gmail.com>
2019-06-02 13:05:43 -0600

    prizes page

--- src/components/prizes.js
+++ src/components/prizes.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import SimpleBottomNavigation from './bottomNavigation';
+import PrizesImage from '../images/prizes.png';
 
 class Prizes extends Component {
   render() {
     return (
       <div>
-        <h1>Prizes Page</h1>
+        <img src={PrizesImage} alt={'logo'} id="image" />
         <SimpleBottomNavigation />
       </div>
     );
