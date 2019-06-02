6f227ff08938b31f8a3b216d7c1479d69fb1de83
Rocky Chima <jatinder.chima@gmail.com>
2019-06-02 13:19:44 -0600

    add buddy png to buddies page

--- src/components/buddies.js
+++ src/components/buddies.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import SimpleBottomNavigation from './bottomNavigation';
+import BuddyImage from '../images/buddy.png';
 
 class Buddies extends Component {
   render() {
     return (
       <div>
-        <h1>Buddies Page</h1>
+        <img src={BuddyImage} alt={'logo'} id="image" />
         <SimpleBottomNavigation />
       </div>
     );
