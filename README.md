77b7a090479828b7859543207aa086910794976b
Rocky Chima <jatinder.chima@gmail.com>
2019-06-03 11:55:54 -0600

    add some context to readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ## Paction
 
+YYC Startup Weekend:
+
+Techstars Startup Weekend is the place to look for a team, create a prototype of your idea, validate your business idea, and receive feedback from experienced entrepreneurs, all in one weekend.
+
 ### Screenshots
 
 <img src="https://github.com/connka/accountabuddies/blob/master/src/images/Paction-Video.gif" width =411 height=823>
--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-// import HomeImage from '../images/home.png';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        {/* <img src={HomeImage} alt={'logo'} id="image" /> */}
         <p />
         <NavLink to={'/dashboard'}>
           <button className="signup-btn">SIGN UP</button>
